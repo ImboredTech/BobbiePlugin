b3ae281c4f454681137147e7ba60ecca69ce6a29
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-05-08 21:26:47 +0100

    Adding all gamemode commands editing plugin.yml to register them. Adding comments to config.yml

--- src/main/java/xyz/foresthosting/bobbieeplugin/BobbieePlugin.java
+++ src/main/java/xyz/foresthosting/bobbieeplugin/BobbieePlugin.java
@@ -3,6 +3,10 @@ package xyz.foresthosting.bobbieeplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.foresthosting.bobbieeplugin.commands.GamemodeAdventure;
+import xyz.foresthosting.bobbieeplugin.commands.GamemodeCreative;
+import xyz.foresthosting.bobbieeplugin.commands.GamemodeSpectator;
+import xyz.foresthosting.bobbieeplugin.commands.GamemodeSurvival;
 import xyz.foresthosting.bobbieeplugin.events.PlayerJoin;
 import xyz.foresthosting.bobbieeplugin.events.PlayerQuit;
 
@@ -30,6 +34,10 @@ public final class BobbieePlugin extends JavaPlugin
     public void loadCommands()
     {
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Loading Commands...");
+        getCommand("gmc").setExecutor(new GamemodeCreative(this));
+        getCommand("gms").setExecutor(new GamemodeSurvival(this));
+        getCommand("gmsp").setExecutor(new GamemodeSpectator(this));
+        getCommand("gma").setExecutor(new GamemodeAdventure(this));
     }
 
     // Loads all events
---
+++ src/main/java/xyz/foresthosting/bobbieeplugin/commands/GamemodeSpectator.java
@@ -0,0 +1,64 @@
+package xyz.foresthosting.bobbieeplugin.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import xyz.foresthosting.bobbieeplugin.BobbieePlugin;
+
+public class GamemodeSpectator implements CommandExecutor
+{
+    private final BobbieePlugin plugin;
+    public GamemodeSpectator(BobbieePlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        // checking to see if the sender is a player
+        if(sender instanceof Player)
+        {
+            // checking to see if the player has permission.
+            if(sender.hasPermission("bobbieplugin.spectator") || sender.hasPermission("bobbieplugin.gamemode"))
+            {
+                // checking to see if they specified a player
+                if(args.length < 1)
+                {
+                    ((Player) sender).setGameMode(GameMode.SPECTATOR);
+                    sender.sendMessage(ChatColor.GREEN + "Your gamemode has been changed to spectator.");
+                }
+                // applying logic when they specify a player.
+                else if(args.length > 1)
+                {
+                    Player player = Bukkit.getPlayer(args[0]);
+                    // checking to see if the player exists ingame.
+                    if(player == null)
+                    {
+                        sender.sendMessage(ChatColor.RED + "Player not online!");
+                    }
+                    // applying logic if they do
+                    else
+                    {
+                        player.setGameMode(GameMode.SPECTATOR);
+                        player.sendMessage(ChatColor.GREEN + "Your gamemode has been changed to spectator.");
+                        sender.sendMessage(ChatColor.GREEN + "The player's gamemode has been changed to spectator.");
+                    }
+                }
+            }
+            // applying logic when they don't have permission.
+            else
+            {
+                sender.sendMessage(ChatColor.RED + "You do not have permission to run this command.");
+            }
+        }
+        // applying logic when they aren't a player.
+        else
+        {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "You must be ingame to run this command.");
+        }
+        return false;
+    }
+}
